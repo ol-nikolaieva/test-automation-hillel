1c7f759a79b44648a38f131e2bf12603df359b88
Olena Nikolaieva <nikolayeva.alyona@gmail.com>
2023-02-09 16:32:01 +0100

    added homework fot the 13th lesson: Locators And Waiters

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,15 +1,25 @@
 import com.hillel.core.driver.WebDriverFactory;
 import com.hillel.util.ConfigProvider;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestRunner {
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
         driver.get(ConfigProvider.BASE_URL);
 
+        WebElement qaAutomationCourse = driver.findElement(By.xpath("//ul[@class='course-nav_list']/li[3]/a[contains(@href, 'automation')]"));
+
+        qaAutomationCourse.click();
+        wait.until(ExpectedConditions.titleContains("QA Automation"));
         driver.quit();
     }
 }
